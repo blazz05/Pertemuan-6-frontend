2495f6c2777692b4018e3c72607b4907bc4e9bbe
Aldi Budiansah <129393970+blazz05@users.noreply.github.com>
2023-11-13 17:10:34 +0700

    tugas6

---
+++ src/utils/constants/DataMovie.js
@@ -0,0 +1,149 @@
+const DataMovie = [
+    {
+        id: 1,
+        title: "Fury",
+        date: "11 November 2014",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pfte7wdMobMF4CVHuOxyu6oqeeA.jpg"
+    },
+    {
+        id: 2,    
+        title: "Venom",
+        date: "05 Oktober 2018",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2uNW4WbgBXL25BAbXGLnLqX71Sw.jpg"
+    },
+    {
+        id: 3,
+        title: "The Marvels",
+        date: "04 Mei 2020",
+        image: "https://www.themoviedb.org/t/p/original/tRk6CmIc1w99b8p9kS7KRU3mZvV.jpg"
+    }
+    ,
+    {
+        id: 4,
+        title: "Lucki",
+        date: "09 November 2023",
+        image: "https://www.themoviedb.org/t/p/original/lnWJtG3vmbU7dNJrYfGk08oMAX.jpg"
+    },
+    {
+        id: 5,
+        title: "Oppenheimer",
+        date: "19 Juli 2023",
+        image: "https://www.themoviedb.org/t/p/original/2AXxc5qrYsix01mD7uyy7PTMnQq.jpg"
+    },
+    {
+        id: 6,
+        title: "Shrek",
+        date: "23 Juni 2004",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2yYP0PQjG8zVqturh1BAqu2Tixl.jpg"
+    },
+    {
+        id: 7,
+        title: "Lucifer",
+        date: "10 September 2021",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ekZobS8isE6mA53RAiGDG93hBxL.jpg"
+    },
+    {
+        id: 8,
+        title: "The Nun II",
+        date: "06 September 2023",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5gzzkR7y3hnY8AD1wXjCnVlHba5.jpg"
+    },
+    {
+        id: 9,
+        title: "Robert Dony Xavier",
+        date: "25 Agustus 2023",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/y6QRQ0bHGt9Wc1BBTZsa1iB2PAm.jpg"
+    },
+    {
+        id: 10,
+        title: "Sniper:G.R.I.T",
+        date: "06 September 2023",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/a9bt9byTQ1MIfRWYQX240HiYPrl.jpg"
+    },
+    {
+        id: 11,
+        title: "Law & Order",
+        date: "18 Mei 2023",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/77OPlbsvX3pzoFbyfpcE3GXMCod.jpg"
+    },
+    {
+        id: 12,
+        title: "Nowhere",
+        date: "29 September",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/zwKaPkkLizCg1onpHQq89LWugkS.jpg"
+    },
+    {
+        id: 13,
+        title: "Rise of the Beasts",
+        date: "09 Juni 2023",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/gPbM0MK8CP8A174rmUwGsADNYKD.jpg"
+    },
+    {
+        id: 15,
+        title: "Impossible-Dead Reckoning",
+        date: "12 Juli 2023",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/NNxYkU70HPurnNCSiCjYAmacwm.jpg"
+    },
+    {
+        id: 16,
+        title: "The Blacklist",
+        date: "13 Juli 2023",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/r935SMphvXppx5bJjbIBNx02fwc.jpg"
+    },
+    {
+        id: 17,
+        title: "Blue Beetle",
+        date: "16 Agustus 2023",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/3Woi0H9vrNy98NWrtL9bM39zSDw.jpg"
+    },
+    {
+        id: 18,
+        title: "The Flash",
+        date: "14 Juni 2023",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/yrrsBUQzBYkPn9btngf5DTHOSyA.jpg"
+    },
+    {
+        id: 19,
+        title: "Castle",
+        date: "16 Mei 2016",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/diXBeMzvfJb2iJg3G0kCUaMCzEc.jpg"
+    },
+    {
+        id: 20,
+        title: "The Equalizer 3",
+        date: "30 Agustus 2023",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/b0Ej6fnXAP8fK75hlyi2jKqdhHz.jpg"
+    },
+    {
+        id: 21,
+        title: "Gen V",
+        date: "02 November 2023",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/uuot1N5AgZ7xRCKgm4ZCwOhgIJu.jpg"
+    },
+    {
+        id: 22,
+        title: "Games Of Thrones",
+        date: "19 Mei 2023",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1XS1oqL89opfnbLl8WnZY1O1uJx.jpg"
+    },
+    {
+        id: 23,
+        title: "Coco",
+        date: "11 November 2019",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/gGEsBPAijhVUFoiNpgZXqRVWJt2.jpg"
+    },
+    {
+        id: 24,
+        title: "Kung Fu Panda",
+        date: "06 Juni 2008",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/wWt4JYXTg5Wr3xBW2phBrMKgp3x.jpg"
+    },
+    {
+        id: 25,
+        title: "Pemikiran Kriminal",
+        date: "09 Februari 2023",
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/7TCwgX7oQKxcWYEhSPRmaHe6ULN.jpg"
+    },
+]
+
+export default DataMovie
